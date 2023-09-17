8df357db45e66956fe6c9d46f7ed27ccd07a7f2b
VadimST04 <vadikstarchak04@gmail.com>
2023-09-17 18:37:31 +0200

    add base authentication

--- backend/baseApp/urls.py
+++ backend/baseApp/urls.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from baseApp import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    # authentication urls
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
+    # other urls
     path('users/', views.UserList.as_view(), name='users'),
 ]
