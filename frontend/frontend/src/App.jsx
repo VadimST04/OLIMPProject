072b94c48f03c9cf08af2946fb4d3ce7508fe583
VadimST04 <vadikstarchak04@gmail.com>
2023-09-17 23:45:02 +0200

    add frontend and its base configurations

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Header from './components/Header'
+import HomePage from "./pages/HomePage";
+import ContactsPage from "./pages/ContactsPage";
+
+const Root = () => {
+  return (
+    <div>
+      <Header />
+      {/* The ContactsPage should be to the right of the Outlet */}
+      <ContactsPage />
+      {/* Outlet is our Application */}
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
