fce76bb1bec7a4caa3572f1cc9e4649a2e91ea1b
VadimST04 <vadikstarchak04@gmail.com>
2023-09-26 22:50:08 +0200

    backend-frontend-users

--- frontend/frontend/src/store/store.jsx
+++ frontend/frontend/src/store/store.jsx
@@ -8,11 +8,13 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { userLoginReducer } from "../store/reducers/userReducers";
 import { postsListReducer } from "../store/reducers/postsReducers";
 import { userProfileReducer } from "../store/reducers/profileReducers";
+import { userListReducer } from "../store/reducers/userReducers";
 
 const reducer = combineReducers({
   userToken: userLoginReducer,
   postsList: postsListReducer,
   userProfile: userProfileReducer,
+  users: userListReducer,
 });
 
 const initialStore = {};
