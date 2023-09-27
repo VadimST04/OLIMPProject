ee9ee4567af22ec8544e9d5d5ee2a0cdcaa1a3b6
VadimST04 <vadikstarchak04@gmail.com>
2023-09-27 16:05:25 +0200

    add booksApp carcas

---
+++ backend/booksApp/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+from baseApp.models import Language
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL)
+    cover_image = models.ImageField()
+    pages_count = models.IntegerField()
+    languages = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return f'{self.title}'
