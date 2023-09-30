e21f31078ebe2ad6811a65291a004c0e4356fcb9
VadimST04 <vadikstarchak04@gmail.com>
2023-09-30 15:52:56 +0200

    newsApp in backend

--- backend/newsApp/models.py
+++ backend/newsApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from baseApp.models import Language
+
+
+class News(models.Model):
+    """
+    This is a class for news that user liked
+    """
+
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, blank=True, null=True)
+    video_url = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(max_length=500)
+    content = models.TextField()
+    pub_date = models.DateTimeField(blank=True, null=True)
+    image_url = models.CharField(max_length=255)
+    source_id = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    language = models.ManyToManyField(Language)
