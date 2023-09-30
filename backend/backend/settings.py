a49f9be8824aaa69b4f5bbab78c4f5af1c40f712
VadimST04 <vadikstarchak04@gmail.com>
2023-10-01 00:17:24 +0200

    more time for token

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 import os
 
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     'newsApp',
     'postsApp',
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
