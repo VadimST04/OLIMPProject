5e4081c31a3418f1e4a80a0481e6517042bcac30
Soviet.Dog <soviet.dog1945@proton.me>
2023-10-09 10:40:21 +0300

    optimize requests

--- backend/booksApp/utils.py
+++ backend/booksApp/utils.py
@@ -12,13 +12,17 @@ def get_data_from_html(book_data: str | requests.Response) -> Dict[str, str]:
     """
     if isinstance(book_data, str):
         book_data = requests.get(book_data)
-    book_data.encoding = 'utf-8'
-    soup = BeautifulSoup(book_data.text, 'html.parser')
-    start_point = r'\*\*\*([\s\S]+?)\*\*\*'
 
-    author = re.split(r'Author: ([\s\S]+?)\n', soup.get_text())[1].strip('\n')
-    title = re.split(r'Title: ([\s\S]+?)\n', soup.get_text())[1].strip('\n')
-    lang = re.split(r'Language: ([\s\S]+?)\n', soup.get_text())[1].strip('\n')
-    text = re.split(start_point, soup.get_text())[2].strip('\n')
+    if book_data.status_code == 200:
+        book_data.encoding = 'utf-8'
+        soup = BeautifulSoup(book_data.text, 'html.parser')
+        start_point = r'\*\*\*([\s\S]+?)\*\*\*'
 
-    return {'text': text, 'lang': lang, 'title': title, 'author': author}
+        author = re.split(r'Author: ([\s\S]+?)\n', soup.get_text())[1].strip('\n')
+        title = re.split(r'Title: ([\s\S]+?)\n', soup.get_text())[1].strip('\n')
+        lang = re.split(r'Language: ([\s\S]+?)\n', soup.get_text())[1].strip('\n')
+        text = re.split(start_point, soup.get_text())[2].strip('\n')
+
+        return {'text': text, 'lang': lang, 'title': title, 'author': author}
+
+    raise requests.exceptions.ConnectionError('Incorrect link')
