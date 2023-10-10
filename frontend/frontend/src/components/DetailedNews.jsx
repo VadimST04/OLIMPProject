9b152813979f17ee9dfbeafb3f82122b301e7bde
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-10-10 15:52:44 +0300

    Update DetailedNews.jsx

--- frontend/frontend/src/components/DetailedNews.jsx
+++ frontend/frontend/src/components/DetailedNews.jsx
@@ -13,23 +13,13 @@ const DetailedNews = ({
   language,
   setOpenDetails,
 }) => {
-  const dateString = "2023-10-10 10:00:34";
   const date = new Date(pub_date);
   const now = new Date();
-
-  const timeDifference = now - date;
-
-  const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60));
-  const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
+  const hoursDifference = Math.floor((now - date) / (1000 * 60 * 60));
+  const daysDifference = Math.floor((now - date) / (1000 * 60 * 60 * 24));
   const timeSinceArticlePosted =
     daysDifference > 0 ? daysDifference + "d" : hoursDifference + "h";
 
-  if (daysDifference > 0) {
-    console.log(`Количество дней: ${daysDifference}d`);
-  } else {
-    console.log(`Количество часов: ${hoursDifference}h`);
-  }
   const url = new URL(link);
   const websiteName = url.hostname.replace("www.", "");
   return (
@@ -49,17 +39,14 @@ const DetailedNews = ({
         <div className="w-full space-y-1">
           <p className="text-[#737373]">{websiteName}</p>
           <p className="text-[22px] font-semibold">{title}</p>
-
           {author && (
             <p className="text-[#737373]">
               Article by {author.join(", ")} &#x2022; {timeSinceArticlePosted}
             </p>
           )}
-
           {!author && (
             <p className="text-[#737373]">{timeSinceArticlePosted} ago</p>
           )}
-
           <p className="text-soft-black">{content}</p>
         </div>
       </div>
