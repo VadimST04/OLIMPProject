ce5580522c469354369fed26ddd0c7a2ef5d626b
Pasichnichenkoilya <54638946+Pasichnichenkoilya@users.noreply.github.com>
2023-10-12 15:25:32 +0300

    Users page (#46)
    * Add basic layout
    
    * Add data from backend

--- frontend/frontend/src/pages/UsersPage.jsx
+++ frontend/frontend/src/pages/UsersPage.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../store/actions/userActions";
+import Useritem from "../components/Useritem";
 
 const UsersPage = () => {
   const dispatch = useDispatch();
-
-  const { users } = useSelector((state) => state.usersList); // need to use SPRED operator (...)
-  console.log(users);
+  const { users } = useSelector((state) => state.usersList); // need to use SPREAD operator (...)
 
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>UsersPage</h1>
-      {users &&
-        users.map((item, index) => (
-          <p key={index}>username: {item.username}</p>
-        ))}
+    <div className="flex h-full w-full flex-wrap gap-x-12 gap-y-5 overflow-y-auto">
+      {users?.map((item, index) => (
+        <div key={index}>
+          <Useritem
+            image={item.image}
+            description={item.description}
+            username={item.user.username}
+          />
+        </div>
+      ))}
     </div>
   );
 };
