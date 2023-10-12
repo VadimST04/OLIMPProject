65ef1811f7f77d4606bf1455262c37f5af7b3755
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-10-12 16:56:38 +0300

    Add detailed post

---
+++ frontend/frontend/src/components/CommentItem.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+const CommentItem = ({ profileImg, username, commentDate, commentContent }) => {
+  const [expandComment, setExpandComment] = useState(false);
+  const commentLineClamp = expandComment ? "" : "line-clamp-1";
+  const maxSymbols = 43;
+  const cursorStyle =
+    commentContent.length > maxSymbols ? "cursor-pointer" : "";
+  return (
+    <div className="flex w-full gap-2 p-2">
+      <div className="h-12 w-12 shrink-0 overflow-hidden rounded-full">
+        <img src={profileImg} alt="" className="h-full w-full object-cover" />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex gap-2">
+          <p className="font-semibold">{username}</p>
+          &#x2022;
+          <p>{commentDate}</p>
+        </div>
+        <p className={`${commentLineClamp}`}>{commentContent}</p>
+        {commentContent.length > maxSymbols && (
+          <div
+            onClick={() => setExpandComment(!expandComment)}
+            className="w-full cursor-pointer hover:text-[#d3d3d3]"
+          >
+            {expandComment ? "Hide" : "Expand"}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CommentItem;
--- frontend/frontend/src/components/DetailedPost.jsx
+++ frontend/frontend/src/components/DetailedPost.jsx
@@ -4,6 +4,9 @@ import { BiComment } from "react-icons/bi";
 import { AiOutlineHeart, AiOutlineSend } from "react-icons/ai";
 import { FiSend } from "react-icons/fi";
 import { LuArrowDownSquare } from "react-icons/lu";
+import CommentItem from "./CommentItem";
+import { RxCross1 } from "react-icons/rx";
+
 const DetailedPost = ({
   images,
   likes,
@@ -16,6 +19,40 @@ const DetailedPost = ({
     if (e.target === postBg.current) closePostCallback();
   };
   const [activeImgIndex, setActiveImgIndex] = useState(0);
+  console.log(comments);
+
+  const testComments = [
+    {
+      profileImg:
+        "https://images.unsplash.com/photo-1696945157988-5dbff7a97d02",
+      commentDate: "Aug 30, 20203",
+      username: "user1",
+      commentContent: "Nice cars!",
+    },
+    {
+      profileImg:
+        "https://images.unsplash.com/photo-1696945157988-5dbff7a97d02",
+      commentDate: "Aug 30, 20203",
+      username: "user2",
+      commentContent:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem mollitia repellendus modi, officia voluptas reiciendis accusamus et distinctio laudantium aut esse sit est odit dolorem quibusdam corporis libero autem delectus.",
+    },
+    {
+      profileImg:
+        "https://images.unsplash.com/photo-1696945157988-5dbff7a97d02",
+      commentDate: "Aug 30, 20203",
+      username: "user3",
+      commentContent:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem mollitia repellendus modi, officia voluptas reiciendis accusamus et distinctio laudantium aut esse sit est odit dolorem quibusdam corporis libero autem delectus.",
+    },
+    {
+      profileImg:
+        "https://images.unsplash.com/photo-1696945157988-5dbff7a97d02",
+      commentDate: "Aug 30, 20203",
+      username: "user4",
+      commentContent: "Lorem ipsum dolor sit amet consecteturrrrrr",
+    },
+  ];
 
   return (
     <div
@@ -23,38 +60,71 @@ const DetailedPost = ({
       onClick={(e) => closePost(e)}
       className="absolute left-0 top-0 z-10 flex h-screen min-h-0 w-screen items-center justify-center overflow-y-auto bg-black bg-opacity-80"
     >
-      <div className="flex">
-        <div className="flex w-[24rem] flex-col gap-2 bg-soft-white">
-          <img
-            src={images[activeImgIndex].image}
-            alt=""
-            className={
-              "h-[32rem] w-full cursor-pointer select-none object-cover"
-            }
-          />
-          <div className="space-y-1 text-main-green">
-            <div className="flex items-center justify-between ">
-              <div className="flex items-center gap-2">
-                <div className="flex cursor-pointer select-none items-center text-center font-bold hover:text-main-dark-green">
-                  <AiOutlineHeart className="text-[28px]" />
-                  <p>{likes}</p>
+      <div className="absolute right-[20%] top-10 z-10 cursor-pointer text-[20px] text-soft-white">
+        <RxCross1 className="" onClick={() => closePostCallback()} />
+      </div>
+      <div className="flex gap-4">
+        {/* Post */}
+        <div className="flex w-[29rem] flex-col">
+          <div
+            className={"flex h-full items-center".concat(
+              images.length > 1 ? " justify-between" : " justify-center",
+            )}
+          >
+            {images.length > 1 && (
+              <BsArrowLeftCircle
+                onClick={() =>
+                  setActiveImgIndex(
+                    (activeImgIndex - 1 + images.length) % images.length,
+                  )
+                }
+                className="cursor-pointer select-none text-[32px] text-soft-white hover:text-[#B3B3B3]"
+              />
+            )}
+
+            <div className="flex h-full w-[24rem] flex-col gap-2">
+              <img
+                src={images[activeImgIndex].image}
+                alt=""
+                className={
+                  "h-[31rem] w-full cursor-pointer select-none object-cover"
+                }
+              />
+              <div className="space-y-1 text-soft-white">
+                <div className="flex items-center justify-between ">
+                  <div className="flex items-center gap-2">
+                    <div className="flex cursor-pointer select-none items-center text-center font-bold hover:text-[#B3B3B3]">
+                      <AiOutlineHeart className="text-[28px]" />
+                      <p>{likes}</p>
+                    </div>
+                    <div className="flex cursor-pointer select-none items-center text-center font-bold hover:text-[#B3B3B3]">
+                      <BiComment className="text-[26px] " />
+                      <p>{comments.length}</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <FiSend className="cursor-pointer text-[25px] hover:text-[#B3B3B3]" />
+                    <LuArrowDownSquare className="cursor-pointer text-[27px] hover:text-[#B3B3B3]" />
+                  </div>
                 </div>
-                <div className="flex cursor-pointer select-none items-center text-center font-bold hover:text-main-dark-green">
-                  <BiComment className="text-[26px] " />
-                  <p>{comments.length}</p>
-                </div>
-              </div>
-              <div className="flex items-center gap-2">
-                <FiSend className="cursor-pointer text-[25px] hover:text-main-dark-green" />
-                <LuArrowDownSquare className="cursor-pointer text-[27px] hover:text-main-dark-green" />
               </div>
             </div>
-            <p className="max-h-32 overflow-y-auto p-1 text-[14px] font-semibold">
-              {content}
-            </p>
+            {images.length > 1 && (
+              <BsArrowRightCircle
+                onClick={() =>
+                  setActiveImgIndex((activeImgIndex + 1) % images.length)
+                }
+                className="cursor-pointer select-none text-[32px] text-soft-white hover:text-[#B3B3B3]"
+              />
+            )}
           </div>
         </div>
-        <div className="w-[24rem] flex-shrink-0 bg-soft-white"></div>
+        {/* Comments */}
+        <div className="w-[24rem] text-soft-white ">
+          {testComments.map((item, index) => (
+            <CommentItem {...item} />
+          ))}
+        </div>
       </div>
     </div>
   );
