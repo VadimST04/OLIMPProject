ded88a0f5492b6e32277e02db0ff15cd3e3ce953
Pasichnichenkoilya <54638946+Pasichnichenkoilya@users.noreply.github.com>
2023-10-21 12:39:55 +0300

    Add music page (#52)

--- frontend/frontend/src/pages/MusicPage.jsx
+++ frontend/frontend/src/pages/MusicPage.jsx
@@ -1,18 +1,284 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { musicList } from "../store/actions/musicActions";
+import MusicItem from "../components/MusicItem";
 
 const MusicPage = () => {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const { music } = useSelector((state) => state.musicList);
-  console.log(music);
+  // const { music } = useSelector((state) => state.musicList);
+  // console.log(music);
 
-  useEffect(() => {
-    dispatch(musicList());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(musicList());
+  // }, [dispatch]);
 
-  return <div>MusicPage</div>;
+  const testMusic = [
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1697325320142-28beaededbf3",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1696595861023-35fde5406cb2",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1692035072849-93a511f35b2c",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+    {
+      language: "English",
+      isLiked: false,
+      image: "https://images.unsplash.com/photo-1695917642455-8ee7e1ce83db",
+      title: "Lorem",
+      author: "Ipsum",
+      length: "4:37",
+    },
+  ];
+
+  return (
+    <div className="flex h-full w-full flex-wrap gap-4 overflow-y-auto">
+      {testMusic.map((item) => (
+        <MusicItem {...item} />
+      ))}
+    </div>
+  );
 };
 
 export default MusicPage;
