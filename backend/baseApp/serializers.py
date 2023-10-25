3e905795a4cfa058d59490c9a09b3ae02fe05e50
VadimST04 <vadikstarchak04@gmail.com>
2023-10-25 23:35:43 +0200

    kill pagination

--- backend/baseApp/serializers.py
+++ backend/baseApp/serializers.py
@@ -5,6 +5,29 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from baseApp.models import UserProfile
 
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    """
+    This serializer extends the TokenObtainPairSerializer from the SimpleJWT library.
+    It adds user-specific data to the token response, expanding the default return.
+    """
+
+    def validate(self, attrs):
+        """
+        This method extends the base validation provided by TokenObtainPairSerializer.
+        It includes additional user-specific data in the response data.
+        :param attrs: The dictionary which contains token and user data.
+        :return: Returns token data and additional fields.
+        """
+        data = super().validate(attrs)
+
+        user_profile = UserProfile.objects.get(user=self.user)
+
+        request = self.context.get('request')
+        if request:
+            data['image'] = request.build_absolute_uri(user_profile.image.url)
+        return data
+
+
 class UserSerializer(serializers.ModelSerializer):
     """
     Serializer for User model objects.
@@ -43,26 +66,3 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         model = UserProfile
         fields = ['user', 'image', 'description', 'app_lang', 'learning_langs']
-
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    """
-    This serializer extends the TokenObtainPairSerializer from the SimpleJWT library.
-    It adds user-specific data to the token response, expanding the default return.
-    """
-
-    def validate(self, attrs):
-        """
-        This method extends the base validation provided by TokenObtainPairSerializer.
-        It includes additional user-specific data in the response data.
-        :param attrs: The dictionary which contains token and user data.
-        :return: Returns token data and additional fields.
-        """
-        data = super().validate(attrs)
-
-        user_profile = UserProfile.objects.get(user=self.user)
-
-        request = self.context.get('request')
-        if request:
-            data['image'] = request.build_absolute_uri(user_profile.image.url)
-        return data
