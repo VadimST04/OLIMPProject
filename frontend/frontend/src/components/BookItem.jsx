201d289b04ddc81cc419b9101372600ec7700f1d
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-10-26 15:16:58 +0300

    Update book page according to figma

--- frontend/frontend/src/components/BookItem.jsx
+++ frontend/frontend/src/components/BookItem.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
-const BookItem = ({ image, title, author }) => {
+const BookItem = ({ image, title, author, language }) => {
   return (
-    <div className="h-60 w-44">
+    <div className="group w-44 cursor-pointer space-y-2 overflow-hidden rounded-2xl border border-[#CACACA] p-2 hover:bg-soft-white-hover dark:border-none dark:bg-[#737373] dark:hover:bg-soft-black-hover">
       <img
         src={image}
         alt=""
-        className="h-full w-full cursor-pointer object-cover transition-all duration-300 hover:scale-110"
+        className="h-52 w-full cursor-pointer select-none rounded-2xl object-cover transition-all duration-300 group-hover:scale-110"
       />
-      <p className="mt-2 w-full truncate text-center">{title}</p>
-      <p className="w-full truncate text-center text-[12px]">{author}</p>
-      <button className="mt-1 w-full rounded-md bg-main-green px-5 py-1 text-soft-white hover:bg-main-dark-green">
-        Read Now
-      </button>
+      <p className="w-full truncate text-center font-bold">{title}</p>
+      <div className="flex w-full items-center justify-center gap-1 truncate text-center text-[14px]">
+        <p>{language}</p>
+        <p>&#x2022;</p>
+        <p>{author}</p>
+      </div>
     </div>
   );
 };
--- frontend/frontend/src/components/NewsItem.jsx
+++ frontend/frontend/src/components/NewsItem.jsx
@@ -34,7 +34,11 @@ const NewsItem = ({
       }}
       className="relative h-full w-full"
     >
-      <img src={image_url} alt="" className="h-full w-full object-cover" />
+      <img
+        src={image_url}
+        alt=""
+        className="h-full w-full select-none object-cover"
+      />
       <div className="group absolute bottom-0 left-0 flex h-full w-full cursor-pointer flex-col justify-end bg-gradient-to-t from-black to-[50%] px-5 pb-5 font-semibold">
         <p className="line-clamp-1 w-full text-[18px] text-soft-white">
           {title}
