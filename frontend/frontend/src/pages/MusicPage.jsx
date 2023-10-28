8870ec053c3eb5828681e9e8d39b9234884adc0a
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-10-29 00:18:12 +0300

    Update MusicPage.jsx

--- frontend/frontend/src/pages/MusicPage.jsx
+++ frontend/frontend/src/pages/MusicPage.jsx
@@ -428,7 +428,11 @@ const MusicPage = () => {
           </div>
           <div className="grid w-full grid-cols-[repeat(auto-fill,minmax(11rem,1fr))] gap-4">
             {testMusic.map((item) => (
-              <MusicItem {...item} key={item.id} />
+              <MusicItem
+                {...item}
+                key={item.id}
+                onClickHandler={musicItemClick}
+              />
             ))}
           </div>
         </div>
