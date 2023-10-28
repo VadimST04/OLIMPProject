227ab450134e7eb6a629507da36ea6b924cf5888
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-10-29 01:23:27 +0300

    Update Chats.jsx
    Add collapse button

--- frontend/frontend/src/components/Chats.jsx
+++ frontend/frontend/src/components/Chats.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import ChatItem from "./ChatItem";
 import { useSelector } from "react-redux";
+import { BsCaretRight } from "react-icons/bs";
 
 const Chats = () => {
   const { userProfile } = useSelector((state) => state.userProfile);
   const [activeChatIndex, setActiveChatIndex] = useState(0);
+  const [chatsCollapsed, setChatsCollapsed] = useState(false);
+  const collapseButtonRotation = chatsCollapsed ? "rotate-180" : "";
+  const chatsWidth = !chatsCollapsed ? "w-[22rem]" : "w-0";
+
   const testImg =
     "https://upload.wikimedia.org/wikipedia/commons/a/ae/Aristotle_Altemps_Inv8575.jpg";
   const chats = [
@@ -29,32 +34,47 @@ const Chats = () => {
     { img: testImg, name: "Lorem", lastMessage: "lorem XD" },
     { img: testImg, name: "Lorem", lastMessage: "lorem XD" },
   ];
-  
+
   return (
-    <div className="w-[22rem] flex-shrink-0 space-y-1 overflow-y-auto pr-2 text-soft-black">
-      {chats.length === 0 && userProfile && (
-        <div className="flex h-full w-full items-center justify-center">
-          <div
-            className={`flex w-full cursor-pointer select-none items-center justify-center gap-2 rounded-md border border-transparent bg-main-green p-5 text-soft-white transition-all duration-100`}
-          >
-            You don't have any chats yet
+    <div className={`relative ${!chatsCollapsed ? "px-6" : ""}`}>
+      <div
+        onClick={() => setChatsCollapsed(!chatsCollapsed)}
+        className="absolute -left-4 top-[50%] cursor-pointer text-[32px] dark:text-soft-white"
+      >
+        <BsCaretRight
+          className={`transition-all duration-300 group-hover:scale-110 ${collapseButtonRotation}`}
+        />
+      </div>
+
+      <div
+        className={`h-full flex-shrink-0 space-y-1 overflow-y-auto text-soft-black transition-all duration-300 ${chatsWidth} ${
+          !chatsCollapsed ? "pr-2" : ""
+        }`}
+      >
+        {chats.length === 0 && userProfile && (
+          <div className="flex h-full w-full items-center justify-center">
+            <div
+              className={`flex w-full cursor-pointer select-none items-center justify-center gap-2 rounded-md border border-transparent bg-main-green p-5 text-soft-white`}
+            >
+              You don't have any chats yet
+            </div>
           </div>
-        </div>
-      )}
-      {!userProfile && (
-        <div className="flex h-full w-full items-center justify-center dark:text-soft-white">
-          <p className="text-[20px] font-semibold">Log in to use chats</p>
-        </div>
-      )}
-      {userProfile &&
-        chats.map((item, index) => (
-          <ChatItem
-            key={index}
-            isActive={index === activeChatIndex}
-            clickCallback={() => setActiveChatIndex(index)}
-            {...item}
-          />
-        ))}
+        )}
+        {!true && (
+          <div className="flex h-full w-full items-center justify-center dark:text-soft-white">
+            <p className="text-[20px] font-semibold">Log in to use chats</p>
+          </div>
+        )}
+        {true &&
+          chats.map((item, index) => (
+            <ChatItem
+              key={index}
+              isActive={index === activeChatIndex}
+              clickCallback={() => setActiveChatIndex(index)}
+              {...item}
+            />
+          ))}
+      </div>
     </div>
   );
 };
