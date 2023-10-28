667225a225ae9b935d70b0a060c0e45b44075aaa
Jalkhet <jmaximuaj@gmail.com>
2023-10-28 21:58:27 +0100

    Add another base(books in progress)

---
+++ Mobile/BookPage/main.py
@@ -0,0 +1,20 @@
+from kivy.lang import Builder
+from kivy.core.window import Window
+from kivymd.app import MDApp
+from kivy.uix.screenmanager import Screen
+from BookPage import Screen
+
+Window.size = (350, 600)
+
+
+class NewsPageWindow(Screen):
+    pass
+
+
+class Example(MDApp):
+    def build(self):
+
+        return Builder.load_file('css.kv')
+
+
+Example().run()
