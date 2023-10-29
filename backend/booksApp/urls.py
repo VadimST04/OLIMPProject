634c2b0487164be16e760d2b2e6671c6d3fdf400
VadimST04 <vadikstarchak04@gmail.com>
2023-10-29 12:07:23 +0100

    add possibility to see all book covers to unauthorized users

--- backend/booksApp/urls.py
+++ backend/booksApp/urls.py
@@ -5,11 +5,11 @@ import booksApp.views.author as author_views
 
 urlpatterns = [
     # urls for books
-    path('create/', book_views.BookCreateView.as_view()),
-    path('view/<int:pk>/', book_views.BookRUDView.as_view()),
-    path('view-all/', book_views.BooksListAPIView.as_view()),
+    path('create/', book_views.BookCreateView.as_view(), name='book-create'),
+    path('view/<int:pk>/', book_views.BookRUDView.as_view(), name='book-rud'),
+    path('view/', book_views.BooksListAPIView.as_view(), name='book-list'),
 
     # urls for authors
-    path('authors/', author_views.AuthorListView.as_view()),
-    path('authors/<int:pk>', author_views.AuthorRUDView.as_view()),
+    path('authors/', author_views.AuthorListView.as_view(), name='author-list'),
+    path('authors/<int:pk>', author_views.AuthorRUDView.as_view(), name='author-rud'),
 ]
