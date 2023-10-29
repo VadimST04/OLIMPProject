d8d0f7ce9c03b1031f4ce7e8c2f50a6f07d3cbab
VadimST04 <vadikstarchak04@gmail.com>
2023-10-29 15:59:12 +0100

    add redux for main button in navbar

---
+++ frontend/frontend/src/store/reducers/buttonsReducers.jsx
@@ -0,0 +1,14 @@
+import { MAIN_BUTTON_CHANGE_NAME } from "../constants/buttonsConstants";
+
+export const mainButtonReducer = (
+  state = { buttonName: `${(<BsNewspaper />)} News` },
+  action,
+) => {
+  switch (action.type) {
+    case MUSIC_REQUEST:
+      return { buttonName: `${action.payload.icon} ${action.payload.btnName}` };
+
+    default:
+      return state;
+  }
+};
