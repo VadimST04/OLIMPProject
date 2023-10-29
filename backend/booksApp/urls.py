519c86393ac9e7942aca256868f39a4441523c5d
VadimST04 <vadikstarchak04@gmail.com>
2023-10-29 11:56:34 +0100

    add possibility to see all book covers to unauthorized users

--- backend/booksApp/urls.py
+++ backend/booksApp/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # urls for books
     path('create/', book_views.BookCreateView.as_view()),
     path('view/<int:pk>/', book_views.BookRUDView.as_view()),
-    path('view/', book_views.BookRUDView.as_view()),
+    path('view-all/', book_views.BooksListAPIView.as_view()),
 
     # urls for authors
-    path('authors/', author_views.AuthorView.as_view()),
-    path('authors/<int:pk>', author_views.AuthorView.as_view()),
+    path('authors/', author_views.AuthorListView.as_view()),
+    path('authors/<int:pk>', author_views.AuthorRUDView.as_view()),
 ]
--- backend/booksApp/views/books.py
+++ backend/booksApp/views/books.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from django.contrib.auth.models import AnonymousUser
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.views import APIView
 
 from booksApp import utils
 from booksApp.models import Book
@@ -19,19 +20,31 @@ class BookCreateView(generics.CreateAPIView):
 
 class BookRUDView(generics.RetrieveUpdateDestroyAPIView):
     """
-    View to get all data about one instance
+    A view for retrieving, updating, and deleting a single Book instance.
+    This view allows authenticated users to retrieve, update, and delete Book objects.
     """
+
     queryset = Book.objects.all()
     serializer_class = BookRUDSerializer
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
-        # book = Book.objects.filter(**kwargs)
+        """
+        Retrieve a single Book instance by its primary key and return its details.
+        This method retrieves a Book instance using its primary key and returns its title, text, author,
+        and languages in the response.
+        """
         book = generics.get_object_or_404(self.get_queryset(), **kwargs)
         return Response({'title': book.title,
                          'text': utils.get_data_from_html(book.text).get('text'),
                          'author': book.author.name,
                          'languages': book.languages.name})
+
+
+class BooksListAPIView(APIView):
+    """
+    View to get all books
+    """
 
     def post(self, request):
         """
