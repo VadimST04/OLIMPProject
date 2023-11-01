1d4ed2ae37089268139d112d70d3a7e2cb0007b6
Soviet.Dog <soviet.dog1945@proton.me>
2023-11-01 21:32:51 +0200

    add test for one bookview

--- backend/tests/book_app_test/book_test.py
+++ backend/tests/book_app_test/book_test.py
@@ -1,16 +1,16 @@
 import json
-from rest_framework.test import APIClient
+import re
 
 import pytest
 
-import tests.utils as utils
 from baseApp.models import Language
 
+BOOK_PART_FOR_TEST = 'JACK DERRINGER'
 pytestmark = pytest.mark.django_db
 
 
 class TestBookModel:
-    all_books_view_endpoint = '/api/books/view/'
+    books_view_endpoint = '/api/books/view/'
 
     def test_all_books_page_authorized(self, api_client, book_factory, regular_user):
         ger = Language.objects.create(name='German')
@@ -19,11 +19,23 @@ class TestBookModel:
         book_factory.create_batch(5, languages=[ger])
         eng_books = book_factory.create_batch(5, languages=[eng])
 
-        response = api_client().post(path=self.all_books_view_endpoint, data={'learning_langs': ['English']},
-                                     HTTPS_AUTHORIZATION='Bearer ' + regular_user.token)
+        response = api_client().post(path=self.books_view_endpoint, data={'learning_langs': ['English']},
+                                     HTTP_AUTHORIZATION='Bearer ' + regular_user.token)
 
         assert response.status_code == 200
 
         response_books = json.loads(response.content)
 
         assert len(eng_books) == len(response_books)
+
+    def test_get_one_book(self, api_client, book_factory, regular_user):
+        book_link_to_test = 'https://gutenberg.org/cache/epub/71996/pg71996-images.html'
+        book = book_factory.create(text=book_link_to_test)
+
+        custom_book_endpoint = f'{self.books_view_endpoint}{book.pk}/'
+
+        response = api_client().get(path=custom_book_endpoint, HTTP_AUTHORIZATION='Bearer ' + regular_user.token)
+
+        assert response.status_code == 200
+        assert re.match(BOOK_PART_FOR_TEST, response.data.get('text'))
+
