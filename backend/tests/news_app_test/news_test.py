c9548e2e77d50557825f62a2f2e74bd27efdfbe8
Soviet.Dog <soviet.dog1945@proton.me>
2023-11-04 19:22:12 +0200

    add end-end tests to news main page

--- backend/tests/news_app_test/news_test.py
+++ backend/tests/news_app_test/news_test.py
@@ -1,4 +1,15 @@
-def test_news_page(client):
-    response = client.post('/api/news/')
 
-    assert response.status_code == 200
+class TestNewsPage:
+    endpoint = '/api/news/'
+
+    def test_news_page_unauthorized(self, api_client):
+        response = api_client().post(self.endpoint, data={'learning_langs': ['English']})
+
+        assert response.status_code == 200
+
+    def test_news_page_authorized(self, api_client, regular_user):
+        response = api_client().post(self.endpoint,
+                                     data={'learning_langs': regular_user.data.user_profile.learning_langs.all()},
+                                     HTTP_AUTHORIZATION=regular_user.token)
+
+        assert response.status_code == 200
