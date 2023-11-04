ba51992d8afa2408e168d86d27e85417ad4c9795
VadimST04 <vadikstarchak04@gmail.com>
2023-11-04 21:59:15 +0100

    update/add song retrieve view

--- backend/musicApp/urls.py
+++ backend/musicApp/urls.py
@@ -4,5 +4,6 @@ from musicApp import views
 urlpatterns = [
     path('', views.SongList.as_view(), name='music-list'),
     path('create/', views.SongSettings.as_view(), name='music-create'),
-    path('<int:pk>/', views.SongSettings.as_view(), name='music-settings'),
+    path('<int:pk>/', views.SongRetrieve.as_view(), name='music-retrieve'),
+    path('settings/<int:pk>/', views.SongSettings.as_view(), name='music-settings'),
 ]
