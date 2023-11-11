02e017576bca41d2748f235212f0427082e6eac9
Soviet.Dog <soviet.dog1945@proton.me>
2023-11-11 23:08:27 +0200

    add admin user fixture

--- backend/tests/fixtures.py
+++ backend/tests/fixtures.py
@@ -35,3 +35,27 @@ def regular_user(client, django_user_model):
 @pytest.fixture
 def api_client():
     return APIClient
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def admin_user(client, django_user_model):
+    username = 'test_admin_user'
+    password = 'test_passw'
+
+    eng = Language.objects.create(name='English')
+    ger = Language.objects.create(name='German')
+
+    admin = django_user_model.objects.create_superuser(
+        username=username, password=password, is_superuser=True)
+
+    test_profile = UserProfile.objects.create(user_id=admin.pk, image='backend/static/images/test.png',
+                                              description=None, app_lang=eng)
+    test_profile.learning_langs.set([eng])
+    test_profile.learning_langs.set([ger])
+
+    response = client.post('/api/login/', {'username': username, 'password': password}, format='json')
+
+    UserData = namedtuple('UserData', ['data', 'token'])
+    user_data = UserData(admin, response.data.get('access'))
+    return user_data
