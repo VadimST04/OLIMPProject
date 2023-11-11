6827ed5121b5b0399c30ea5c77539a9f2b50dc38
Soviet.Dog <soviet.dog1945@proton.me>
2023-11-11 23:07:58 +0200

    add song and language factory

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,8 +1,10 @@
 from pytest_factoryboy import register
 
-from tests.factories import BookFactory, AuthorFactory
+from tests.factories import BookFactory, AuthorFactory, SongFactory, LanguageFactory
 
 pytest_plugins = 'tests.fixtures'
 
 register(BookFactory)
 register(AuthorFactory)
+register(SongFactory)
+register(LanguageFactory)
--- backend/tests/factories.py
+++ backend/tests/factories.py
@@ -2,11 +2,19 @@ import factory
 
 from baseApp.models import Language
 from booksApp.models import Book, Author
+from musicApp.models import Song
 
 
 class AuthorFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Author
+
+    name = factory.Sequence(lambda n: f'test_name{n}')
+
+
+class LanguageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Language
 
     name = factory.Sequence(lambda n: f'test_name{n}')
 
@@ -20,7 +28,7 @@ class BookFactory(factory.django.DjangoModelFactory):
     author = factory.SubFactory(AuthorFactory)
     cover_image = None
 
-    @factory.post_generation # разобрать как работает
+    @factory.post_generation  # разобрать как работает
     def languages(self, create, extracted, **kwargs):
         if not create:
             # Simple build, do nothing.
@@ -30,3 +38,27 @@ class BookFactory(factory.django.DjangoModelFactory):
             # A list of items were passed in, use them
             for lang in extracted:
                 self.languages.add(lang)
+
+
+class SongFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Song
+
+    title = factory.Sequence(lambda n: f'test_song_{n}')
+    artist = factory.Sequence(lambda n: f'test_artist_{n}')
+    image = None
+    audio_file = None
+    audio_link = factory.Sequence(lambda n: f'test__{n}')
+    duration = factory.Sequence(lambda n: f'test_duration_{n}')
+
+    @factory.post_generation  # разобрать как работает
+    def language(self, create, extracted=('English',), **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of items were passed in, use them
+            for lang in extracted:
+                self.language.add(lang)
+
