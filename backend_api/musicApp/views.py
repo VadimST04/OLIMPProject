58e97b916f8132fb36814abc2755a0b770cb8b3f
VadimST04 <vadikstarchak04@gmail.com>
2023-11-15 20:19:36 +0100

    add python-filters to installed

--- backend_api/musicApp/views.py
+++ backend_api/musicApp/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, filters
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from musicApp.models import Song
