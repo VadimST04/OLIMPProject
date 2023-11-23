1f1a582586cb37b0877603608d3578a29b02759a
Pasichnichenkoilya <54638946+Pasichnichenkoilya@users.noreply.github.com>
2023-11-24 00:08:01 +0200

    Add image loader (#89)
    * Add image loader
    
    * Update Chats.jsx
    
    Changed collapse button left property

--- frontend/frontend/src/components/BookItem.jsx
+++ frontend/frontend/src/components/BookItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ImageLoader from "./ImageLoader";
 
 const BookItem = ({
   image,
@@ -21,11 +22,9 @@ const BookItem = ({
       }}
       className="group w-full cursor-pointer space-y-2 overflow-hidden rounded-2xl border border-[#CACACA] p-2 hover:bg-soft-white-hover dark:border-none dark:bg-[#737373] dark:hover:bg-soft-black-hover"
     >
-      <img
-        src={image}
-        alt=""
-        className="h-52 w-full cursor-pointer select-none rounded-2xl object-cover transition-all duration-300 group-hover:scale-110"
-      />
+      <div className="h-52 w-full cursor-pointer select-none overflow-hidden rounded-2xl object-cover transition-all duration-300 group-hover:scale-110">
+        <ImageLoader src={image} />
+      </div>
       <p className="w-full truncate text-center font-bold">{title}</p>
       <div className="flex w-full items-center justify-center gap-1 truncate text-center text-[14px]">
         <p>{language}</p>
