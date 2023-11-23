4f9b4cb64f1de8e30e48c0f91a3ca05c7d02ee2a
VadimST04 <vadikstarchak04@gmail.com>
2023-11-23 17:40:16 +0100

    add short_name to language model

--- backend_api/baseApp/auto_language_migrate.py
+++ backend_api/baseApp/auto_language_migrate.py
@@ -1,12 +1,22 @@
 from django.apps import apps
 from django.db import migrations
 
-LANGUAGES = ['English', 'Spanish', 'German', 'Ukrainian', 'French', 'Italian']
+LANGUAGES = {
+    'English': 'eng',
+    'German': 'deu',
+    'French': 'fre',
+    'Italian': 'ita',
+    'Hebrew': 'heb',
+    'Ukrainian': 'ukr',
+    'Turkish': 'tur',
+    'Spanish': 'spa',
+    'Swedish': 'swe',
+}
 
 
 def create_default_languages():
     language = apps.get_model('baseApp', 'Language')
 
-    for lang in LANGUAGES:
-        if not language.objects.filter(name=lang):
-            language.objects.create(name=lang)
+    for lang_name, lang_short_name in LANGUAGES.items():
+        if not language.objects.filter(name=lang_name):
+            language.objects.create(name=lang_name, short_name=lang_short_name)
--- backend_api/baseApp/models.py
+++ backend_api/baseApp/models.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
-
 from baseApp.auto_language_migrate import create_default_languages
 
 
 class Language(models.Model):
     objects = models.Manager()
     name = models.CharField(max_length=50)
+    short_name = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.name}'
