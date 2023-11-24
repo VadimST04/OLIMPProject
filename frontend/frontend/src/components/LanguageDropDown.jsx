661885fb2d3413b1c2ebbe0d10e9623b4d865a7b
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-11-24 23:18:23 +0200

    Fix size of navbar icons

--- frontend/frontend/src/components/LanguageDropDown.jsx
+++ frontend/frontend/src/components/LanguageDropDown.jsx
@@ -38,7 +38,7 @@ const LanguageDropDown = ({ openUpwards = false }) => {
       className="group relative flex h-10 w-full items-center justify-start gap-2 rounded-md text-soft-white sm:w-10 sm:justify-center sm:hover:bg-main-dark-green"
     >
       <p className="sm:hidden">Languages</p>
-      <MdLanguage className="text-2xl transition-transform duration-150 group-hover:scale-[115%]" />
+      <MdLanguage className="text-[28px] transition-transform duration-150 group-hover:scale-[115%]" />
       <div
         className={`absolute right-0  z-[1] flex max-h-64 w-max cursor-pointer overflow-y-auto rounded-md bg-soft-white ${dropDownVisibility} ${openStyle}`}
       >
--- frontend/frontend/src/components/ThemeToggle.jsx
+++ frontend/frontend/src/components/ThemeToggle.jsx
@@ -20,8 +20,8 @@ const ThemeToggle = () => {
       className="group relative flex h-10 w-full items-center justify-start gap-2 rounded-md text-soft-white sm:w-10 sm:justify-center sm:hover:bg-main-dark-green"
     >
       <p className="sm:hidden">Theme</p>
-      <BsMoonFill className="text-xl transition-transform duration-150 group-hover:scale-[115%] dark:hidden" />
-      <BsFillSunFill className="hidden text-2xl transition-transform duration-150 group-hover:scale-[115%] dark:block" />
+      <BsMoonFill className="text-[22px] transition-transform duration-150 group-hover:scale-[115%] dark:hidden" />
+      <BsFillSunFill className="hidden text-[28px] transition-transform duration-150 group-hover:scale-[115%] dark:block" />
     </button>
   );
 };
