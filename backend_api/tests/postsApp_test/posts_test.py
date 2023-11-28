b1176a1cad4d770f8aa972a443fc5ec967d9fa21
Soviet.Dog <soviet.dog1945@proton.me>
2023-11-28 21:09:06 +0200

    add test comments count on

--- backend_api/tests/postsApp_test/posts_test.py
+++ backend_api/tests/postsApp_test/posts_test.py
@@ -42,4 +42,24 @@ class TestPostsPage:
         content = json.loads(response.content)
         assert len(content.get('comments')) == 10
 
-    # def test
+    # def test_update_post(self, api_client, regular_user, image_post_factory, post_factory):
+    #     post = post_factory.create(user=regular_user.data)
+    #     image_post_factory.create(post=post)
+    #
+    #     new_data_post = {}
+
+
+class TestComment:
+
+    endpoint = '/api/posts/comment/'
+
+    def test_comment_create(self, image_post_factory, post_factory, regular_user, api_client):
+        post = post_factory(user=regular_user.data)
+        image_post_factory.create(post=post)
+
+        new_comment_data = {'text': 'new_text', 'post': post}
+
+        response = api_client().post(self.endpoint, data=new_comment_data,
+                                     HTTP_AUTHORIZATION='Bearer ' + regular_user.token)
+
+        assert response.status_code == 201
