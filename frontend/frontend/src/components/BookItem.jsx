84b758c13ed6862c6624bb98a3fd838db215075f
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-12-05 15:21:38 +0200

    Add skeleton loader to books page

--- frontend/frontend/src/components/BookItem.jsx
+++ frontend/frontend/src/components/BookItem.jsx
@@ -37,4 +37,24 @@ const BookItem = ({
   );
 };
 
+export const BookItemSkeleton = () => {
+  return (
+    <div className="group w-full cursor-pointer space-y-2 overflow-hidden rounded-2xl border border-[#CACACA] p-2 hover:bg-soft-white-hover dark:border-none dark:bg-[#737373] dark:hover:bg-soft-black-hover">
+      <div className="h-52 w-full animate-[pulse_1s_ease-in-out_infinite] cursor-pointer select-none rounded-2xl bg-soft-black dark:bg-soft-white"></div>
+      <div className="flex gap-2">
+        <p className="h-1 w-[50%] animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+        <p className="h-1 w-full animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+        <p className="h-1 w-[20%] animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+        <p className="h-1 w-[30%] animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+      </div>
+      <div className="flex w-full items-center justify-center gap-1 truncate text-center text-[14px]">
+        <p className="h-1 w-[50%] animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+        <p className="h-1 w-[30%] animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+        <p className="h-1 w-[20%] animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+        <p className="h-1 w-full animate-[pulse_1s_ease-in-out_infinite] truncate rounded-full bg-soft-black text-center font-bold dark:bg-soft-white"></p>
+      </div>
+    </div>
+  );
+};
+
 export default BookItem;
