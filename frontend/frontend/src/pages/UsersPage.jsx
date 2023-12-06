b368d1a286aed2a5f62c0edcdaf61df6be47d431
Pasichnichenkoilya <sheredan2003@gmail.com>
2023-12-06 18:43:30 +0200

    Add users loader

--- frontend/frontend/src/pages/UsersPage.jsx
+++ frontend/frontend/src/pages/UsersPage.jsx
@@ -1,18 +1,21 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../store/actions/userActions";
-import Useritem from "../components/Useritem";
+import Useritem, { UseritemSkeleton } from "../components/Useritem";
 
 const UsersPage = () => {
   const dispatch = useDispatch();
-  const { users } = useSelector((state) => state.usersList);
-  
+  const { users, loading, error } = useSelector((state) => state.usersList);
+
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
   return (
     <div className="flex h-full w-full flex-wrap gap-x-12 gap-y-5 overflow-y-auto">
+      {loading &&
+        [...Array(5)].map((_, index) => <UseritemSkeleton key={index} />)}
+
       {users?.map((item, index) => (
         <div key={index}>
           <Useritem
